da4a3e6f2bbb42dff4c54206e6962c7311f715da
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-08-25 20:36:38 -0300

    iniciando pela logica em js

---
+++ Inicio/dados.js
@@ -0,0 +1,16 @@
+var nome = "caneta"
+var quantidade = 10
+var imposto = 1.6
+var preco = 1.10
+var precoFinal = preco + imposto
+
+
+
+console.log(nome)
+console.log(quantidade)
+console.log(imposto)
+console.log(preco)
+console.log(precoFinal)
+
+nome = "Caneta BIC"
+console.log(nome)
