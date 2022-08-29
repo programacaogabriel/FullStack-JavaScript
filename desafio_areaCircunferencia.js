9c9a50cd6b6aa92d877868f302e64ca5936cbd58
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-08-29 14:28:28 -0300

    iniciando pela logica em js

---
+++ desafio_areaCircunferencia.js
@@ -0,0 +1,10 @@
+// pi * (raio*raio)
+
+const { match } = require("assert");
+
+const pi = Math.PI
+let raio = 10;
+
+let area = pi*(raio*raio)
+
+console.log('O valor da area é ' +area+ 'm2' )
---
+++ desafio_trocaValores.js
@@ -0,0 +1,12 @@
+let a = 8;
+let b = 95;
+
+// realizar a troca dos valores das variaveis
+
+let temp = a
+ a = b;
+ b =temp;
+
+ // [a,b] = [b,a]
+
+console.log('Valor de a '+a+' Valor de b '+b)
