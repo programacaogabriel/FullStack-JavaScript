d4335f8a1a614a3f4e530906edb2733fbc242153
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-08-29 13:48:43 -0300

    iniciando pela logica em js

---
+++ Inicio/dados3.js
@@ -0,0 +1,7 @@
+let idade = 35;
+console.log(typeof idade)
+
+
+let salario = 4456.81
+console.log(typeof salario)
+
---
+++ Inicio/dados4.js
@@ -0,0 +1,8 @@
+let a = 3;
+
+// modificando 
+
+a = 10;
+console.log(a)
+
+const b =11 // onstante é uma valor que não pode ser alterado
