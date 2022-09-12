7b84f7941e4fa4cffeaf5b7fb4d7fe33e9c77943
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-12 10:15:23 -0300

    :package: Objetos
    Criando objetos

---
+++ Fundamentos/object.js
@@ -0,0 +1,16 @@
+const prod = {}
+
+prod.nome = 'Celular ultraMega'
+prod.preco = 45678
+prod['desconto_legal'] = 0.45 // evitar colocar espaços entre as palavras
+
+console.log(prod)
+
+
+const prod2 = {
+    camiseta :"Polo JK",
+    preco : 10.58,
+    tamanho : 'G',
+    cor: 'azul'
+}
+console.log(prod2)
