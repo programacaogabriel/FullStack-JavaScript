7a93baf8fdbdaaa2983a7efc5079040a2804e786
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-14 10:07:21 -0300

    :package: Usando let em loop
    Criando exemplos de let em loop com a impressão do I fora do laço

---
+++ Fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 10; i++) {
+
+    console.log(i)
+}
+
+console.log("i= ", i)  // Quando sai do laço o I tem que ser diferente do valor de for, pois está em um bloco. Quando sair do laço for
+// ele será uma numero superior
+
+console.log('\n')
+for (var i = 0; i < 6; i++) {
+    console.log(i)
+}
+console.log("i= ", i)
