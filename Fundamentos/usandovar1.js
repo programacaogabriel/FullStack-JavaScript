08742ce6964704764c5dc9bbd44d960ec9d85e54
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-14 08:39:45 -0300

    :package: Usando Variaveis
    Criando exemplos de variaveis do escopo global

---
+++ Fundamentos/usandovar1.js
@@ -0,0 +1,15 @@
+{ { { { var sera = "sera!" } } } }
+console.log(sera)
+
+// na var o escopo é global ela sempre poderá ser acessada de 2 mananeiras dentro e fora
+
+
+function teste() {
+    var local =123
+    console.log(local)
+}
+// na função o escopo só pode ser acessado dentro dele
+
+
+// =====  quando criamos uma variavel global a mesma sera adicionada ao window ====//
+// var a = "teste"   ficaria no navegador window.a
