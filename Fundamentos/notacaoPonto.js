11c16097c5ea767a09edb824d73515633b43b207
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-15 08:04:14 -0300

    :package: Anotação Ponto
    Exemplos de anotação Ponto

---
+++ Fundamentos/notacaoPonto.js
@@ -0,0 +1,23 @@
+console.log(typeof console)
+
+console.log(Math.ceil(6.1))
+
+
+
+const obj1 = {}
+obj1.nome = "Gabriel"
+// obj1['nome1] = "Gabriel" visto em aulas anteriores, esse metodo não é muito usado
+console.log(obj1.nome)
+
+function Obj(nome){
+    this.nome = nome
+    this.exec = function(){
+        console.log("Exec")
+    }
+}
+
+const obj2 =  new Obj('cadeira')
+const obj3 = new Obj('mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj3.exec()
