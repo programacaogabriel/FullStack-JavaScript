226d44384f1b876f7c77b45e4f26926acd6ea9b9
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-18 16:46:32 -0300

    :package: Ternario
    Exemplos criados de operadores ternarios("Aprovado/Reprovado")

---
+++ Fundamentos/ternario.js
@@ -0,0 +1,9 @@
+//unarioa = Um operando
+//binario = Dois operandos
+//ternario = Tres operandos
+
+const resultado = nota => nota >= 7 ? "Aprovado":"Reprovado"
+
+
+console.log(resultado(7.1))
+console.log(resultado(5.1))
