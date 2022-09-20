31601fb592760f2deee893e78f25c7078c0e8a47
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-20 07:30:26 -0300

    :package: If Else
    Exemplos criados para mostrar o funcionamentos do if else com notas de alunos

--- Estruturas de Controle/if2.js
+++ Estruturas de Controle/if2.js
@@ -1,8 +1,12 @@
 function nota(num) {
-    if (num >= 7)
+    if (num >= 7)               // abrir os parenteses pois o tipo de escrita irá afetar a tudo,Nunca cometer esse erro
         console.log("Teste")
-        console.log("Teste2")
+
+    console.log("Teste2")
 }
 
 nota(6)
 nota(8)
+
+
+
---
+++ Estruturas de Controle/ifElse.js
@@ -0,0 +1,11 @@
+const imprimirResultado = function (valor) {
+    if(valor>=7){
+        console.log("Aprovado")
+    }else {
+        console.log("Reprovado")
+    }
+}
+
+
+imprimirResultado(7)
+imprimirResultado(4)
