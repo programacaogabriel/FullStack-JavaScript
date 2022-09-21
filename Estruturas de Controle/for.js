4ae9d7f45d1776a1cc03ffd92981762109fc0529
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-09-21 08:26:33 -0300

    :package: For
    Exemplos criados de laços de repetição com notas e numeros

---
+++ Estruturas de Controle/for.js
@@ -0,0 +1,16 @@
+let contador = 1
+while(contador <=10 ){
+    console.log(` contador =${contador}`)
+    contador++
+}
+
+for (let i = 1; i<=10; i++) {
+   console.log(`i = ${i}`)
+    
+}
+
+const notas = [7.8, 8.0,7.4,3.7,7.7,6.0]
+
+for(i=0; i< notas.length; i++){
+    console.log(`Notas = ${notas[i]}`)
+}
