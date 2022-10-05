926dd64434986396f0d6586117d87978eb1afb52
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-10-05 09:45:01 -0300

    :package: Exercicio4
    ​Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado

---
+++ Lista de exercícios Fundamentos, Estruturas de controle e Funções/Exericios Feitos/Exercicio4.js
@@ -0,0 +1,10 @@
+/*04) ​Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.*/
+
+const { abort } = require("process");
+
+function dividendoDivisor(a, b) {
+    return a % b
+}
+
+console.log(dividendoDivisor(10,3))
