cc01794e3f66757455ccb4749add2f7a02b92e4b
programacaogabriel <programacaogabriel@gmail.com>
2022-11-03 15:06:17 -0300

    :package: filter 2
    Funcionamento interno do filter

---
+++ Array/filter2.js
@@ -0,0 +1,28 @@
+Array.prototype.filter2 = function(callback){
+
+    const newArray = []
+    for (let i =0; i<this.length; i++){
+        if(callback(this[i], i,this)){
+            newArray.push(this[i])
+        }
+    } 
+    return newArray
+}
+
+
+
+const produtos = [
+    { nome: 'Notebooks', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Cop de Plástico', preco: 18.99, fragil: false }
+
+]
+
+
+//Econtrar Fragil com valor maior de 500
+
+const caro = produtos => produtos.preco >= 500
+const fragil = produtos => produtos.fragil
+
+console.log(produtos.filter2(caro).filter2(fragil))
