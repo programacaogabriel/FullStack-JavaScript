a466ee2d9f3e2b65d4b24bb44d68bf6ed0d7b817
programacaogabriel <programacaogabriel@gmail.com>
2022-11-07 16:28:30 -0300

    :package: concat
    Exemplos que retorna um novo array contendo todos os arrays ou valores passados como parâmetro

---
+++ Array/concat.js
@@ -0,0 +1,10 @@
+const filhas = ['valeskah', 'Cibalena']
+
+const filhos = ['Washington','Wescleitin']
+
+const todos = filhos.concat(filhos)
+
+ console.log(todos,filhas,filhos)
+
+
+ console.log([].concat([1,2],[3,4],5,[[6,7]]))
